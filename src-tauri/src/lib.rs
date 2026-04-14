24b813132278389eece087022ead2fb1de474abe
satrong <satrong@foxmail.com>
2026-04-14 12:28:14 +0800

    feat: add tauri-plugin-updater dependency and configuration

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -9,6 +9,7 @@ pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_dialog::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .invoke_handler(tauri::generate_handler![
             // 配置
             config::load_config,
